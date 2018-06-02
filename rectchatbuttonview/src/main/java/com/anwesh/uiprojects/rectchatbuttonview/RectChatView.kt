9ce9f49f90ccd6ba30f3e4edd7e280f98a0ac1b3
Anwesh43 <anweshthecool0@gmail.com>
2018-06-02 23:59:43 +0530

    adding State class

--- rectchatbuttonview/src/main/java/com/anwesh/uiprojects/rectchatbuttonview/RectChatView.kt
+++ rectchatbuttonview/src/main/java/com/anwesh/uiprojects/rectchatbuttonview/RectChatView.kt
@@ -11,18 +11,37 @@ import android.graphics.*
 
 class RectChatView(ctx : Context) : View(ctx) {
 
-    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-    override fun onDraw(canvas : Canvas) {
+    override fun onDraw(canvas: Canvas) {
 
     }
 
-    override fun onTouchEvent(event : MotionEvent) : Boolean {
+    override fun onTouchEvent(event: MotionEvent): Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
 
             }
         }
         return true
+    }
+
+    data class State(var prevScale : Float = 0f, var dir : Float = 0f, var j : Int = 0) {
+
+        private val scales : Array<Float> = arrayOf(0f, 0f)
+
+        fun update(stopcb : (Float) -> Unit) {
+            scales[j] += dir * 0.1f
+            if (Math.abs(scales[j] - prevScale) > 1) {
+                scales[j] = prevScale + dir
+                j += dir.toInt()
+                if (j == scales.size || j == -1) {
+                    j -= dir.toInt()
+                    dir = 0f
+                    prevScale = scales[j]
+                    stopcb(prevScale)
+                }
+            }
+        }
     }
 }
