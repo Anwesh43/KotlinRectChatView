19e2278af00d64bc0f3f5e345641eb8287ff68f5
Anwesh43 <anweshthecool0@gmail.com>
2018-06-03 03:52:37 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinrectchatview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinrectchatview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.kotlinrectchatview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.rectchatbuttonview.RectChatView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        RectChatView.create(this)
     }
 }
