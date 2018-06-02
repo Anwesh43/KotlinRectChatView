905430ba8a9a10ea202a5a167ceabe77ddddd888
Anwesh43 <anweshthecool0@gmail.com>
2018-06-02 23:32:01 +0530

    adding RectChatViiew class

--- rectchatbuttonview/src/main/java/com/anwesh/uiprojects/rectchatbuttonview/RectChatView.kt
+++ rectchatbuttonview/src/main/java/com/anwesh/uiprojects/rectchatbuttonview/RectChatView.kt
@@ -3,3 +3,26 @@ package com.anwesh.uiprojects.rectchatbuttonview
 /**
  * Created by anweshmishra on 02/06/18.
  */
+
+import android.view.View
+import android.content.Context
+import android.view.MotionEvent
+import android.graphics.*
+
+class RectChatView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
